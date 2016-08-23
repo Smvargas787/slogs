27ae65edb3b46001c28f96cfdcd9bed036246020
Selena <Smvargas787@users.noreply.github.com>
2016-08-22 22:10:18 -0400

    Updated Log Tests
    Adjusted Log Tests to locate streams being displayed.

--- test/__util.js
+++ test/__util.js
@@ -1,6 +1,12 @@
 const util = require('../src/util');
+const expect = require('chai').expect;
 
+
+// Debug Tool
 describe('debug test', () => {
-//
-  it('testing to see if stub displays', () => {});
+  //
+  it('Testing to see if stub displays', (done) => {
+    expect(util.out1).to.not.equal(null);
+    done();
+  });
 });
